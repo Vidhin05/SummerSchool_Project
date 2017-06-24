b76b65a2fde6ddd27ec7d33c5ee17d10827b89ee
Vidhin <ividhin@gmail.com>
2017-06-24 19:38:43 +0530

    removed jquery for now, we may have to remove certain js files, have a look at it.

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 	        easingType: 'linear', // Scroll to top easing (see http://easings.net/)
 	        animation: 'fade', // Fade, slide, none
 	        animationSpeed: 200, // Animation in speed (ms)
-	        scrollTrigger: false, // Set a custom triggering element. Can be an HTML string or jQuery object
+	        scrollTrigger: false, // Set a custom triggering element. Can be an HTML string
 					//scrollTarget: false, // Set a custom target element for scrolling to the top
 	        scrollText: '<i class="fa fa-angle-up"></i>', // Text for element, can contain HTML
 	        scrollTitle: false, // Set a custom <a> title if required.
